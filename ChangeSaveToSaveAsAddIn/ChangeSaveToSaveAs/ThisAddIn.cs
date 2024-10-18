580255bbcefa9198a9aa3f5affb20259eab4f28c
asasascao <asasascao@gmail.com>
2024-10-18 14:10:09 +0800

    whole tool

---
+++ ChangeSaveToSaveAsAddIn/ChangeSaveToSaveAs/ThisAddIn.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Word = Microsoft.Office.Interop.Word;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Word;
+using Microsoft.Office.Interop.Word;
+using System.Windows.Forms;
+
+namespace ChangeSaveToSaveAs
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            Word.Application wordApp = this.Application;
+            wordApp.DocumentBeforeSave += WordApp_DocumentBeforeSave;
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+            Word.Application wordApp = this.Application;
+            wordApp.DocumentBeforeSave -= WordApp_DocumentBeforeSave;
+        }
+
+        private void WordApp_DocumentBeforeSave(Word.Document Doc, ref bool SaveAsUI, ref bool Cancel)
+        {
+            //显示保存框
+            this.Application.Dialogs[WdWordDialog.wdDialogFileSaveAs].Show();
+            //取消保存
+            Cancel = true;
+        }
+
+        #region VSTO 生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+
+        #endregion
+    }
+}
---
+++ ChangeSaveToSaveAsAddIn/ChangeSaveToSaveAsAddInWeb/Home.js
@@ -0,0 +1,34 @@
+﻿
+(function () {
+    "use strict";
+
+    var messageBanner;
+
+    // 每次加载新页面时都必须运行初始化函数。
+    Office.initialize = function (reason) {
+
+        Office.context.document.addHandlerAsync(Office.EventType.DocumentBeforeSave, handleBeforeSave, function (result) {
+            if (result.status === Office.AsyncResultStatus.Failed) {
+                console.error(result.error.message);
+            }
+        });
+    };
+
+    // 这是处理 DocumentBeforeSave 事件的函数
+    function handleBeforeSave(event) {
+        // 在这里，你可以添加你想在保存文档前执行的代码
+        console.log('文档即将保存');
+
+        // 调用Word的另存为对话框
+        Office.context.document.saveAsync(Office.SaveOptions.saveAs, function (result) {
+            if (result.status === Office.AsyncResultStatus.Failed) {
+                console.error(result.error.message);
+            } else {
+                console.log('另存为对话框已打开。');
+            }
+        });
+
+        // 如果你想阻止保存事件，可以设置 event.completed 为 false
+        event.completed();
+    }
+})();
